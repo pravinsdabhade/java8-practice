2d14a4b0854ffd71867f05a92b7472c84b20a9ba
pravinsdabhade <pravinsdabhade44@yahoo.in>
2022-12-31 20:38:49 +0530

    Add files via upload

---
+++ Employee.java
@@ -0,0 +1,74 @@
+package com.practice.model;
+
+import java.util.Objects;
+
+public class Employee {
+
+    private int id;
+    private String name;
+    private double salary;
+
+    private String city;
+
+    public Employee(int id, String name, double salary, String city) {
+        this.id = id;
+        this.name = name;
+        this.salary = salary;
+        this.city = city;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", salary=" + salary + '\''+
+                ", city=" + city +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Employee)) return false;
+        Employee employee = (Employee) o;
+        return getId() == employee.getId() && Double.compare(employee.getSalary(), getSalary()) == 0 && Objects.equals(getName(), employee.getName());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId(), getName(), getSalary());
+    }
+}
